30ec9a81684876b33e08e719218e74f965ff4806
Userraja <raja.bbu.12345543@gmail.com>
2024-10-04 14:21:56 +0600

    Update youtube.js

--- scripts/commands/youtube.js
+++ scripts/commands/youtube.js
@@ -66,52 +66,7 @@ module.exports.handleReply = async function ({
     args: t
 }) {
     const n = global.nodemodule.axios,
-        s = global.nodemodule["fs-extra"],
-        i = (global.nodemodule.request, await n.get("https://raw.githubusercontent.com/MOHAMMAD-NAYAN/Nayan/main/video.json")),
-        r = i.data.keyVideo.length,
-        o = i.data.keyVideo[Math.floor(Math.random() * r)],
-        d = (global.nodemodule["ytdl-core"], global.nodemodule["simple-youtube-api"]),
-        {
-            createReadStream: m,
-            createWriteStream: l,
-            unlinkSync: h,
-            statSync: c
-        } = global.nodemodule["fs-extra"];
-    var u = ["AIzaSyB5A3Lum6u5p2Ki2btkGdzvEqtZ8KNLeXo", "AIzaSyAyjwkjc0w61LpOErHY_vFo6Di5LEyfLK0", "AIzaSyBY5jfFyaTNtiTSBNCvmyJKpMIGlpCSB4w", "AIzaSyCYCg9qpFmJJsEcr61ZLV5KsmgT1RE5aI4"];
-    const g = u[Math.floor(Math.random() * u.length)],
-        p = new d(g);
-    if (0 == t.length || !t) return e.sendMessage("» Search cannot be left blank!", a.threadID, a.messageID);
-    const y = t.join(" ");
-    if (0 == t.join(" ").indexOf("https://")) {
-        var f = {
-            method: "GET",
-            url: "https://ytstream-download-youtube-videos.p.rapidapi.com/dl",
-            params: {
-                id: t.join(" ").split(/^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*/)[3]
-            },
-            headers: {
-                "x-rapidapi-host": "ytstream-download-youtube-videos.p.rapidapi.com",
-                "x-rapidapi-key": `${o.API_KEY}`
-            }
-        };
-        var b = (await n.request(f)).data,
-            v = b.title;
-        if ("fail" == b.status) return e.sendMessage("Unable to send this file.", a.threadID);
-        var k = Object.keys(b.link)[1],
-            I = b.link[k][0];
-        path1 = __dirname + "/cache/1.mp4";
-        const i = (await n.get(`${I}`, {
-            responseType: "arraybuffer"
-        })).data;
-        return s.writeFileSync(path1, Buffer.from(i, "utf-8")), s.statSync(__dirname + "/cache/1.mp4").size > 26e6 ? e.sendMessage("The file could not be sent because it is larger than 25MB..", a.threadID, (() => h(__dirname + "/cache/1.mp4")), a.messageID) : e.sendMessage({
-            body: `» ${v}`,
-            attachment: s.createReadStream(__dirname + "/cache/1.mp4")
-        }, a.threadID, (() => s.unlinkSync(__dirname + "/cache/1.mp4")), a.messageID)
-    }
-    try {
-        const t = global.nodemodule["fs-extra"],
-            n = global.nodemodule.axios;
-        var w = [],
+        sst, await n
             _ = "",
             D = 0,
             S = 0,
